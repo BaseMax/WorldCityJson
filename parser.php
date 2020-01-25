6453f75ee966a6646e944944af62683ba967b76a
Max Base <Max@Asrez.com>
2020-01-26 02:30:30 +0330

    create parser file

---
+++ parser.php
@@ -0,0 +1,25 @@
+<?php
+$_countryCode=file_get_contents("countryCode.json");
+$_countryCode=json_decode($_countryCode, true);
+// print_r($_countryCode);
+$countryCode=[];
+foreach($_countryCode as $item) {
+	$countryCode[$item["code"]]=$item["name"];
+}
+print_r($countryCode);
+$_cityData=file_get_contents("cityData.json");
+$_cityData=json_decode($_cityData, true);
+// print_r($_cityData);
+$cities=[];
+foreach($_cityData as $item) {
+	// $cities[$item["country"]][]=[
+	// 	$item["name"],
+	// 	$item["population"]
+	// ];
+	$cities[]=[
+		$countryCode[$item["country"]] . ", " . $item["name"] ." (".$item["country"] . ")",
+		$item["population"]
+	];
+}
+// print_r($cities);
+file_put_contents("result.json", json_encode($cities));
